dfe7437b23de7bdbcedde4aa263c31dd97c759c3
Rahul Monde <rahul.monde@globant.com>
2019-06-20 13:57:05 +0530

    modified index.js file

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ exports.handler = async (event) => {
     let {name,title} = event;
     return {
         status: 200,
-        msg: 'Welcome ' + title + ' ' + name + ' to the world of AWS version 1'
+        msg: 'Welcome ' + title + ' ' + name + ' to the world of AWS version 1.0'
     }
 };
